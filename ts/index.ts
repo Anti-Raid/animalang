bd1e1387e0c39e40c9e74793fb9d487e4dd88e7d
Your Name <you@example.com>
2026-07-11 19:17:44 +0530

    initial commit

---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+export * from './anima';
+export { impl as implRvm } from './bytecode-rvm/meta'
+export { impl as implRvmCps } from './bytecode-rvm-cps/meta'
+export { impl as implSvm } from './bytecode-svm/meta'
+export * as common from './common'
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({ 
+      insertTypesEntry: true, // Creates a types field reference in your entry point
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    lib: {
+      // Defines the main entry file for your library
+      entry: resolve(__dirname, 'ts/index.ts'),
+      name: 'animalang',
+      // Output formats to generate
+      formats: ['es'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // Ensure third-party dependencies aren't bundled into your library
+      external: [], 
+      output: {
+        globals: {}
+      }
+    }
+  },
+});
