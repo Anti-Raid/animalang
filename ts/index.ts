69d68d5271f6c168584303bdac60fe9975b91d74
cheesycod <cheesycod@outlook.com>
2026-09-20 21:01:05 -0400

    export Cons+BuiltinFunction

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,8 @@
 export * from './anima';
 export { impl as implRvm } from './bytecode-rvm/meta'
 export { Table } from './table';
+export { BuiltinFunction } from './std';
+export { Cons } from './list';
 
 export * as common from './common'
 export { isTruthy, ErrorObject } from './common'
